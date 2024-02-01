d85aadf02e0b32bf879dc45a279d20ad4fd1ea8f
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-01 21:59:19 +0300

    feat: add handleCmdExecution function to execute function based on input cmd

---
+++ src/utils/handleCmdExecution.js
@@ -0,0 +1,51 @@
+import { moveToDir, moveUpDir, showFileList } from './navigation/index.js';
+
+import {
+  copyFile,
+  createFile,
+  moveFile,
+  readFile,
+  removeFile,
+  renameFile,
+} from './file_operations/index.js';
+import { getSystemInfo } from './os_info/index.js';
+import { calculateHash } from './hash_calc/index.js';
+
+export async function handleCmdExecution(cmd, args) {
+  switch (cmd) {
+    case 'up':
+      moveUpDir();
+      break;
+    case 'cd':
+      moveToDir(...args);
+      break;
+    case 'ls':
+      await showFileList();
+      break;
+    case 'cat':
+      await readFile(...args);
+      break;
+    case 'add':
+      await createFile(...args);
+      break;
+    case 'rn':
+      await renameFile(args);
+      break;
+    case 'cp':
+      await copyFile(args);
+      break;
+    case 'rm':
+      await removeFile(...args);
+      break;
+    case 'mv':
+      await moveFile(args);
+      break;
+    case 'os':
+      getSystemInfo(...args);
+      break;
+    case 'hash':
+      await calculateHash(...args);
+    default:
+      break;
+  }
+}
