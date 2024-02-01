6f0002230998d0a4cf178346f9cc867ac0fc940d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-01 21:58:08 +0300

    feat: add getSystemInfo functions

---
+++ src/utils/os_info/index.js
@@ -0,0 +1,56 @@
+import { EOL, arch, cpus, homedir, userInfo } from 'os';
+
+import { handleInvalidInputMsg } from '../messages/index.js';
+
+function getEOL() {
+  console.log(JSON.stringify(EOL));
+}
+
+function getCPUs() {
+  const [{ model }] = cpus();
+  const totalAmount = cpus().length;
+  //divide by 1000 turn MHz to GHz
+  const clockRates = cpus().map((cpu) => cpu.speed / 1000 + ' GHz');
+  const output = {
+    model: model.trim(),
+    totalAmount,
+    clockRates,
+  };
+
+  console.log(output);
+}
+
+function getHomeDir() {
+  console.log(homedir());
+}
+
+function getSystemUsername() {
+  console.log(userInfo().username);
+}
+
+function getCPUArchitecture() {
+  console.log(arch());
+}
+
+export function getSystemInfo(arg) {
+  switch (arg) {
+    case '--EOL':
+      getEOL();
+      break;
+    case '--cpus':
+      getCPUs();
+      break;
+    case '--homedir':
+      getHomeDir();
+      break;
+    case '--username':
+      getSystemUsername();
+      break;
+    case '--architecture':
+      getCPUArchitecture();
+      break;
+    default:
+      handleInvalidInputMsg();
+      break;
+  }
+}
