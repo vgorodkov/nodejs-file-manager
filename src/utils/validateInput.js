6ce73c2f25f8bbc7c25033de04ccc38da3fdda02
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-01 21:59:39 +0300

    feat: add validateInput function

---
+++ src/utils/validateInput.js
@@ -0,0 +1,18 @@
+export function validateInput(cmd, args) {
+  const allowedCmds = {
+    '.exit': 0,
+    up: 0,
+    cd: 1,
+    ls: 0,
+    cat: 1,
+    add: 1,
+    rn: 2,
+    cp: 2,
+    rm: 1,
+    mv: 2,
+    os: 1,
+    hash: 1,
+  };
+
+  return args.length === allowedCmds[cmd] || false;
+}
