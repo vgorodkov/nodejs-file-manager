78cdd9c1111ab61ed043dbe7031b8157d1813335
vgorodkov <gorodkov.victor@gmail.com>
2024-02-05 16:58:54 +0300

    feat: add handleOperationFailedMsg

--- src/utils/messages/index.js
+++ src/utils/messages/index.js
@@ -1,17 +1,22 @@
-import { strings } from '../../constants/strings.js';
+import { strings } from "../../constants/strings.js";
+import { EOL } from "os";
 
 export function handleUserGreetingMsg(username) {
-  console.log(`${strings.greeting}, ${username}!\n`);
+  console.log(`${strings.greeting}, ${username}!${EOL}`);
 }
 
 export function handleUserLeavingMsg(username) {
-  console.log(`${strings.farewell}, ${username}, goodbye!\n`);
+  console.log(`${strings.farewell}, ${username}, goodbye!${EOL}`);
 }
 
 export function handleCwdMsg() {
-  console.log(`${strings.cwd} ${process.cwd()}\n`);
+  console.log(`${strings.cwd} ${process.cwd()}${EOL}`);
 }
 
 export function handleInvalidInputMsg() {
-  console.error(strings.invalidInput + '\n');
+  console.error(strings.invalidInput);
+}
+
+export function handleOperationFailedMsg() {
+  console.error(strings.executionError);
 }
