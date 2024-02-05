9d946cc3f6cdf4170d17623270b03823e7c906ce
vgorodkov <gorodkov.victor@gmail.com>
2024-02-05 16:56:52 +0300

    feat: add default username if one's not provided

--- src/utils/parseUsername.js
+++ src/utils/parseUsername.js
@@ -1,14 +1,14 @@
 export function parseUsername() {
   const args = process.argv.slice(2);
-  let username = '';
+  let username = "";
 
   for (let i = 0; i < args.length; i += 1) {
     const arg = args[i];
 
-    if (arg.startsWith('--username')) {
-      username = arg.split('=')[1];
+    if (arg.startsWith("--username")) {
+      username = arg.split("=")[1];
     }
   }
 
-  return username.trim();
+  return username?.trim() || "User";
 }
