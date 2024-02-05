55af9465e1e43fb7f642765b37a42e4a00330797
vgorodkov <gorodkov.victor@gmail.com>
2024-02-05 16:57:37 +0300

    feat: wrap readFile with Promise to correctly console.log cwd after operation is done

--- src/utils/file_operations/index.js
+++ src/utils/file_operations/index.js
@@ -1,28 +1,33 @@
-import { createReadStream, createWriteStream } from 'fs';
-import { strings } from '../../constants/strings.js';
-import { access, constants, rename, rm, stat, writeFile } from 'fs/promises';
-import { dirname, join } from 'path';
-import { cwd } from 'process';
+import { createReadStream, createWriteStream } from "fs";
+import { strings } from "../../constants/strings.js";
+import { access, constants, rename, rm, stat, writeFile } from "fs/promises";
+import { basename, dirname, join } from "path";
+import { cwd } from "process";
+import { handleOperationFailedMsg } from "../messages/index.js";
+import { EOL } from "os";
 
 export async function readFile(path) {
-  try {
-    await access(path, constants.R_OK | constants.W_OK);
+  return new Promise((resolve) => {
     const sourceFromFile = createReadStream(path);
     sourceFromFile.pipe(process.stdout);
-  } catch (err) {
-    if ((err.code = 'ENOENT')) {
-      console.error(strings.invalidInput);
-      return;
-    }
-    console.error(strings.executionError);
-  }
+
+    sourceFromFile.on("end", () => {
+      console.log(EOL);
+      resolve();
+    });
+
+    sourceFromFile.on("error", () => {
+      handleOperationFailedMsg();
+      resolve();
+    });
+  });
 }
 
 export async function createFile(fileName) {
   try {
-    await writeFile(join(cwd(), fileName), '', { flag: 'wx' });
+    await writeFile(join(cwd(), fileName), "", { flag: "wx" });
   } catch {
-    console.error(strings.executionError);
+    handleOperationFailedMsg();
   }
 }
 
@@ -32,7 +37,7 @@ export async function renameFile(args) {
   try {
     await rename(sourceFilePath, join(dirname(sourceFilePath), targetFileName));
   } catch (err) {
-    console.error(strings.executionError);
+    handleOperationFailedMsg();
   }
 }
 
@@ -41,30 +46,35 @@ export async function copyFile(args) {
   const target = args[1];
   try {
     await access(source, constants.R_OK | constants.W_OK);
-    await access(target, constants.R_OK | constants.W_OK);
+    const fileName = basename(source);
     const fileStats = await stat(source);
     const isFile = fileStats.isFile();
     if (!isFile) {
-      console.error('Operation failed');
+      handleOperationFailedMsg();
       return;
     }
-    createReadStream(source).pipe(createWriteStream(join(target, source)));
-  } catch (err) {
-    console.error(strings.executionError);
+    createReadStream(source).pipe(createWriteStream(join(target, fileName)));
+  } catch {
+    handleOperationFailedMsg();
   }
 }
 
 export async function removeFile(filePath) {
   try {
     await rm(filePath);
-  } catch (err) {
-    console.error(strings.executionError);
+  } catch {
+    handleOperationFailedMsg();
   }
 }
 
 export async function moveFile(args) {
   const source = args[0];
 
-  await copyFile(args);
-  await removeFile(source);
+  try {
+    await access(source, constants.F_OK | constants.W_OK);
+    await copyFile(args);
+    await removeFile(source);
+  } catch {
+    handleOperationFailedMsg();
+  }
 }
