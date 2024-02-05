4bb1911c432fe4024610d28357a2737b3e2d9579
vgorodkov <gorodkov.victor@gmail.com>
2024-02-05 16:58:38 +0300

    refacotr: delete not used imports

--- src/utils/hash_calc/index.js
+++ src/utils/hash_calc/index.js
@@ -1,22 +1,23 @@
-import { strings } from '../../constants/strings.js';
-import { createHash } from 'crypto';
-import { createReadStream } from 'fs';
-import { EOL } from 'os';
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { EOL } from "os";
+import { handleOperationFailedMsg } from "../messages/index.js";
 
 export function calculateHash(sourcePath) {
   return new Promise((resolve) => {
-    const hash = createHash('sha256');
+    const hash = createHash("sha256");
     const source = createReadStream(sourcePath);
 
-    source.pipe(hash.setEncoding('hex')).pipe(process.stdout);
+    source.pipe(hash.setEncoding("hex")).pipe(process.stdout);
 
-    source.on('end', () => {
+    source.on("end", () => {
       console.log(EOL);
       resolve();
     });
 
-    source.on('error', () => {
-      console.error(strings.executionError);
+    source.on("error", () => {
+      handleOperationFailedMsg();
+      resolve();
     });
   });
 }
