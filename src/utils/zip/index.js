f74dddea71e6b513dd1405ad68c834ac46ea498e
vgorodkov <gorodkov.victor@gmail.com>
2024-02-05 17:00:08 +0300

    fix: fix compress/decompress function incorrect dest fileName

--- src/utils/zip/index.js
+++ src/utils/zip/index.js
@@ -1,14 +1,15 @@
-import { createBrotliCompress, createBrotliDecompress } from 'zlib';
-import { createReadStream, createWriteStream } from 'fs';
+import { createBrotliCompress, createBrotliDecompress } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
 
-import { strings } from '../../constants/strings.js';
-import { access, constants } from 'fs/promises';
-import { basename, join } from 'path';
+import { access, constants } from "fs/promises";
+import { basename, extname, join } from "path";
+import { handleOperationFailedMsg } from "../messages/index.js";
+import { pipeline } from "stream/promises";
 
 export async function compressFile(args) {
   const sourcePath = args[0];
   const fileName = basename(sourcePath);
-  const targetPath = join(args[1], `${fileName}.gz`);
+  const targetPath = join(args[1], `${fileName}.br`);
   const brotli = createBrotliCompress();
 
   try {
@@ -19,30 +20,28 @@ export async function compressFile(args) {
 
     source.pipe(brotli).pipe(target);
   } catch (err) {
-    if ((err.code = 'ENOENT')) {
-      console.error(strings.invalidInput);
-      return;
-    }
-    console.error(strings.executionError);
+    handleOperationFailedMsg();
   }
 }
 
 export async function decompressFile(args) {
   const sourcePath = args[0];
+  const sourceFileExt = extname(sourcePath);
 
-  const targetPath = join(args[1], `decompressed.txt`);
-  const brotli = createBrotliDecompress();
+  if (sourceFileExt !== ".br") {
+    handleOperationFailedMsg();
+    return;
+  }
+
   try {
+    const targetFilename = basename(sourcePath).slice(0, -3);
+    const targetPath = join(args[1], targetFilename);
+    const brotli = createBrotliDecompress();
     await access(sourcePath, constants.R_OK | constants.W_OK);
     const source = createReadStream(sourcePath);
     const target = createWriteStream(targetPath);
-
-    source.pipe(brotli).pipe(target);
-  } catch {
-    if ((err.code = 'ENOENT')) {
-      console.error(strings.invalidInput);
-      return;
-    }
-    console.error(strings.executionError);
+    await pipeline(source, brotli, target);
+  } catch (e) {
+    handleOperationFailedMsg();
   }
 }
